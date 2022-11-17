a7acc01593a2c56e8b635592e262bcb4b07f45d5
Alhusly10 <sulaimanabdulmuheez@gmail.com>
2022-11-17 17:26:49 +0100

    Changing of title and addition of favicon

--- script.js
+++ script.js
@@ -2,10 +2,20 @@ var MenuItems = document.getElementById("MenuItems")
 
 MenuItems.style.maxHeight = "0px"
 
-const menutoggle = function () {
+ function  menutoggle() {
     if (MenuItems.style.maxHeight == "0px") {
-        MenuItems.style.accentColor.maxHeight = "200px"
+        MenuItems.style.maxHeight = "200px"
     } else {
         MenuItems.style.maxHeight = "0px"
     }
 }
+
+var i = 0;
+var numbers = '';
+
+while (i < 10) {
+    console.log("it is increment at:" + i);
+    i++;
+}
+
+console.log(numbers)
